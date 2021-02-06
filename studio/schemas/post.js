15a0e92f608feb6c3d7adbcadcbfb9345af40eaf
Christian Farris <christianfarriscs@gmail.com>
2021-02-06 17:37:43 -0600

    create POST type

---
+++ studio/schemas/post.js
@@ -0,0 +1,48 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'mainImage',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'preview',
+      title: 'Preview',
+      type: 'text',
+      rows: '4',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96
+      },
+      validation: Rule => Rule.required()
+    },
+  ],
+}
