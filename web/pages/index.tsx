c2f2d136965012467a4a26f8e5e1a239c668e3b9
Christian Farris <christianfarriscs@gmail.com>
2021-02-07 16:27:37 -0600

    simplify index

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,39 +1,11 @@
-import React from 'react';
-import { GetStaticProps } from 'next';
-// import { ABtn } from 'aspire-components-react';
-import { ReactElement } from 'react';
-import { gql, useQuery } from '@apollo/client';
+import React, { ReactElement } from 'react';
 
-import styles from '../styles/Home.module.css';
 import AppMenu from '../components/AppMenu';
 
 const Home = (): ReactElement => {
-  const { data, loading } = useQuery(gql`
-    query Users {
-      users {
-        uuid
-        id
-        email
-      }
-    }
-  `);
-
-  if (loading) return <h1>loading...</h1>;
-
-  if (data)
-    return (
-      <div>
-        <h1>Hello</h1>
-        <p>{JSON.stringify(data)}</p>
-      </div>
-    );
-
   return (
-    <div className={styles.container}>
+    <div>
       <AppMenu />
-      {/* <ABtn align={'center'} justify={'center'}>
-        {'Hello World'}
-      </ABtn> */}
     </div>
   );
 };
