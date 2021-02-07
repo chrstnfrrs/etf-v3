ac7dfa6fff12629455c9be78d2335f46e9dc17ae
Christian Farris <christianfarriscs@gmail.com>
2021-02-07 11:39:29 -0600

    remove out dir

--- web/out/_next/static/iyAhzessk_uAhasUdnqTX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F9ac62baa638228d52db3.css","static\u002Fchunks\u002Fpages\u002Findex-19f4f938135e4c7af3e2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-85785763783c1e33a9f3.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
