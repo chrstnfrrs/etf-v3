b5a5ed5cb39a53107edaa8d63f0f95b82b4d0b3a
Christian Farris <christianfarriscs@gmail.com>
2021-02-07 16:27:27 -0600

    add global css to _app

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import withApollo from 'next-with-apollo';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
+import '../globals.css';
+
 // eslint-disable-next-line react/prop-types
 const App = ({ Component, pageProps, apollo }) => (
   <ApolloProvider client={apollo}>
