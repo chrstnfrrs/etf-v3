3596aa7ef06c1386a1cc547c524aa48452bbf0f4
Christian Farris <christianfarriscs@gmail.com>
2021-02-06 18:08:37 -0600

    eslint changes

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,24 +1,27 @@
 module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'get-off-my-lawn',
+    'plugin:@typescript-eslint/recommended', // Use recommended rules from @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable conficting rules
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. MUST ALWAYS BE LAST.
+  ],
   parser: '@typescript-eslint/parser', // Use TS Parser
   parserOptions: {
-    ecmaVersion: 2020, // Allow modern ECMAScript
-    sourceType: 'module', // Allow imports
     ecmaFeatures: {
       jsx: true, // Allows for the parsing of JSX
     },
+    ecmaVersion: 2020, // Allow modern ECMAScript
+    sourceType: 'module', // Allow imports
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    'react/react-in-jsx-scope': 'off',
+    'unicorn/no-abusive-eslint-disable': 'off',
   },
   settings: {
     react: {
       version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
     },
-  },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended', // Use recommended rules from @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable conficting rules
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. MUST ALWAYS BE LAST.
-  ],
-  rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
   },
 };
