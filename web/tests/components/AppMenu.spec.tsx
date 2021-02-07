66b54a5ce888a186e9679c4ff136fdb909f6ec4a
Christian Farris <christianfarriscs@gmail.com>
2021-02-07 16:27:51 -0600

    only import render

--- web/tests/components/AppMenu.spec.tsx
+++ web/tests/components/AppMenu.spec.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import * as RTL from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { AppMenu } from '../../components/AppMenu';
 
 describe('<AppMenu />', () => {
   test('should do a thing', () => {
-    const result = RTL.render(<AppMenu />);
+    const result = render(<AppMenu />);
 
     expect(result).toBeDefined();
   });
