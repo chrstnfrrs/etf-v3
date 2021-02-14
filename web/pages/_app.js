b64a0cc06d6f1f54929fb95c7e40f313fbba1654
Christian Farris <christianfarriscs@gmail.com>
2021-02-13 20:58:01 -0600

    add sanity url

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -2,18 +2,20 @@ import React from 'react';
 import withApollo from 'next-with-apollo';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
+import AppMenu from '../components/AppMenu';
 import '../globals.css';
 
 // eslint-disable-next-line react/prop-types
 const App = ({ Component, pageProps, apollo }) => (
   <ApolloProvider client={apollo}>
+    <AppMenu />
     <Component {...pageProps} />
   </ApolloProvider>
 );
 
 export default withApollo(({ initialState }) => {
   return new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
+    uri: process.env.SANITY_URL,
     cache: new InMemoryCache().restore(initialState || {}),
   });
 })(App);
