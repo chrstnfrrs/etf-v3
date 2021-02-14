906f3898227657e8f39f34161ba26a4dc4a17692
Christian Farris <christianfarriscs@gmail.com>
2021-02-13 20:58:50 -0600

    Test building block components and start building homepage

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,13 +1,43 @@
 import React, { ReactElement } from 'react';
+import { useRouter } from 'next/router';
 
-import AppMenu from '../components/AppMenu';
+import { AHero, AHeading } from 'aspire-components-react';
+
+import ALoader from '../components/ALoader';
+import { useAllPagesQuery } from '../graphql/generated';
 
 const Home = (): ReactElement => {
-  return (
-    <div>
-      <AppMenu />
-    </div>
-  );
+  const router = useRouter();
+
+  const { data, loading, error } = useAllPagesQuery({
+    variables: {
+      route: router.pathname,
+    },
+  });
+
+  if (error) router.push('error');
+
+  if (data && data.allPages && data.allPages[0] && data.allPages[0].sections) {
+    return (
+      <div>
+        {data.allPages[0].sections.map((section) => {
+          if (section.__typename === 'Hero') {
+            console.log(section.heroImage.asset.url)
+
+            return (
+              <AHero key={section._key} backgroundImage={section.heroImage.asset.url}>
+                <AHeading as={'h1'}>{section.heading}</AHeading>
+              </AHero>
+            );
+          }
+          return null;
+        })}
+        <AHeading type={'h6'}>Heading things</AHeading>
+      </div>
+    );
+  }
+
+  return <ALoader />;
 };
 
 export default Home;
