b1424bc72942c0e9b6dce16126c8ea73608f4eff
Christian Farris <christianfarriscs@gmail.com>
2021-02-18 19:43:38 -0600

    add button to ignored types

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -9,12 +9,12 @@ export default () =>
         .child(
           S.document()
             .schemaType('menu')
-            .documentId('hommenuePage')
+            .documentId('homemenuPage')
         ),
       
       // Add a visual divider (optional)
       S.divider(),
       // List out the rest of the document types, but filter out the config type
       ...S.documentTypeListItems()
-        .filter(listItem => !['menu', 'link', 'hero'].includes(listItem.getId()))
+        .filter(listItem => !['menu', 'link', 'hero', 'button'].includes(listItem.getId()))
     ])
