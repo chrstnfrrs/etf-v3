06b93bc1d8162766484b7f0e9c85b8d7abf2f64f
Christian Farris <christianfarriscs@gmail.com>
2021-02-20 15:20:12 -0600

    allow cors

--- web/functions/email.js
+++ web/functions/email.js
@@ -1,6 +1,11 @@
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({success: true})
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    },
+    body: JSON.stringify({ success: true }),
   };
-}
+};
