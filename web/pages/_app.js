287f0d1446d25cdf57b63d654ab78000de1a8889
Christian Farris <christianfarriscs@gmail.com>
2021-02-20 20:52:36 -0600

    test setTheme

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -2,18 +2,27 @@ import React from 'react';
 import Head from 'next/head';
 import withApollo from 'next-with-apollo';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+// import { setTheme } from 'aspire-components-react';
 
 import '../globals.css';
 
 // eslint-disable-next-line react/prop-types
-const App = ({ Component, pageProps, apollo }) => (
-  <ApolloProvider client={apollo}>
-    <Head>
-      <link rel="shortcut icon" href="/favicon.ico" />
-    </Head>
-    <Component {...pageProps} />
-  </ApolloProvider>
-);
+const App = ({ Component, pageProps, apollo }) => {
+  // React.useEffect(() => {
+  //   setTheme({
+  //     color: '#fff',
+  //   });
+  // }, []);
+
+  return (
+    <ApolloProvider client={apollo}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+};
 
 export default withApollo(({ initialState }) => {
   return new ApolloClient({
