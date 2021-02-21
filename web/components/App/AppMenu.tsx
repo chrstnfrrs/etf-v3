3f1004d0b322c3f224927a5ad70b1cb18b48bb8d
Christian Farris <christianfarriscs@gmail.com>
2021-02-20 20:54:05 -0600

    Seperate App Menu into multiple lists of links

--- web/components/App/AppMenu.tsx
+++ web/components/App/AppMenu.tsx
@@ -1,26 +1,24 @@
-import Link from 'next/link';
 import React, { ReactElement } from 'react';
-import { ANav } from 'aspire-components-react';
+import { ALink, ANav } from 'aspire-components-react';
 import { MenuLinks } from '../../types/app';
 
-const AppMenu = (props: MenuLinks): ReactElement => {
-  if (props?.links) {
-    return (
-      <ANav>
-        {props.links.map((link) => (
-          <Link key={link._key} href={link.route}>
-            {link.text}
-          </Link>
-        ))}
-      </ANav>
-    );
-  }
-
-  return (
-    <ANav>
-      <Link href={'/'}>Erin Tallard Fitness</Link>
-    </ANav>
-  );
-};
+const AppMenu = (props: MenuLinks): ReactElement => (
+  <ANav justify={'between'}>
+    <div>
+      {props.leftLinks?.map((link) => (
+        <ALink key={link._key} href={link.route} className={'menu-link'}>
+          {link.text}
+        </ALink>
+      ))}
+    </div>
+    <div>
+      {props.rightLinks?.map((link) => (
+        <ALink key={link._key} href={link.route} className={'menu-link'} decorators={link?.decorator}>
+          {link.text}
+        </ALink>
+      ))}
+    </div>
+  </ANav>
+);
 
 export default AppMenu;
