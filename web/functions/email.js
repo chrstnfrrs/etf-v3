0592ebce24f903402432f550d9982a5b5568b5f8
Christian Farris <christianfarriscs@gmail.com>
2021-02-21 16:57:46 -0600

    have email send emails

--- web/functions/email.js
+++ web/functions/email.js
@@ -1,8 +1,45 @@
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+function createEmailTemplate(email, name, message) {
+  const template = `
+    <div><span><strong>Name: </strong>${name}</span></div>
+    <div><span><strong>Email: </strong>${email}</span></div>
+    <div><span><strong>Message: </strong>${message}</span></div>
+  `;
+
+  return template;
+}
+
 exports.handler = async function (event, context) {
   try {
-    console.log('Sending Email.');
+    const body = JSON.parse(event.body);
 
+    console.log('body', body);
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: 'christianfarriscs@gmail.comv',
+      subject: `CONTACT INQUIRY - ${body.subject}`,
+      html: createEmailTemplate(body.email, body.name, body.message),
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+
+    res.setHeader('Content-Type', 'application/json');
     return {
+      data: { success: true },
       statusCode: 200,
     };
   } catch (error) {
