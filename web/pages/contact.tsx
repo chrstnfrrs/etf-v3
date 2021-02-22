84c026933ab84e6ecc02430101a15f0c34ff21a6
Christian Farris <christianfarriscs@gmail.com>
2021-02-21 19:01:16 -0600

    contact page testing

--- web/pages/contact.tsx
+++ web/pages/contact.tsx
@@ -3,22 +3,51 @@ import React, { ReactElement, useState } from 'react';
 import { AContainer, AHeading, AInput } from 'aspire-components-react';
 
 const Contact = (): ReactElement => {
-  const [email, setEmail] = useState('');
+  const [input, setInput] = useState({
+    email: '',
+    message: '',
+    name: '',
+    subject: '',
+  });
 
-  const sendEmail = () => {
-    fetch('https://www.erintallardfitness.com/.netlify/functions/email').then((data) => {
-      console.log(data);
+  const sendEmail = async () => {
+    const data = await fetch('https://www.erintallardfitness.com/.netlify/functions/email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(input),
     });
+
+    console.log('data', data);
   };
 
   return (
     <AContainer>
       <AHeading>Contact Us</AHeading>
       <AInput
+        label={'Name'}
+        placeholder={'example@example.com'}
+        value={input.name}
+        onChange={(e) => setInput({ ...input, name: e.target.value })}
+      />
+      <AInput
         label={'Email'}
         placeholder={'example@example.com'}
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        value={input.email}
+        onChange={(e) => setInput({ ...input, email: e.target.value })}
+      />
+      <AInput
+        label={'Subject'}
+        placeholder={'example@example.com'}
+        value={input.subject}
+        onChange={(e) => setInput({ ...input, subject: e.target.value })}
+      />
+      <AInput
+        label={'Message'}
+        placeholder={'example@example.com'}
+        value={input.message}
+        onChange={(e) => setInput({ ...input, message: e.target.value })}
       />
       <button onClick={sendEmail}>Here</button>
     </AContainer>
