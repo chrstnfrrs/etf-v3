18c990b24e574761e8e44c9481d55f2b3e4a52af
Christian Farris <christianfarriscs@gmail.com>
2021-02-21 19:57:54 -0600

    blog page testing

--- web/pages/blog/index.tsx
+++ web/pages/blog/index.tsx
@@ -1,10 +1,15 @@
 import React, { ReactElement } from 'react';
+import BlockContent from '@sanity/block-content-to-react';
+import { AContainer, AHeading, ASection, ALink } from 'aspire-components-react';
 
-import { AContainer, AHeading, ASection, AText } from 'aspire-components-react';
-
+import withLayout from '../../components/App/withLayout';
 import { getGraphqlClient } from '../../graphql/utils';
+import { AllPostPreviewDocument } from '../../graphql/generated';
+import { getLinks } from '../../utils/menu';
 
-const BlogPage = (): ReactElement => {
+const BlogPage = (props): ReactElement => {
+  console.log(props.posts)
+
   const arr = ['asdf', 'asdf', 'asdf', 'asdf', 'asdf'];
 
   const bodySample = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel mi et eros malesuada sagittis sodales at mi. Fusce nec felis finibus, faucibus odio et, mollis ligula. Vestibulum posuere gravida ipsum nec accumsan. Donec non interdum libero, at tempor mauris. Praesent ex nunc, rutrum a orci quis, imperdiet vestibulum metus. Vestibulum non velit justo. Praesent consectetur mattis augue at rutrum. Ut vitae mattis sapien. Curabitur tristique dolor sit amet massa.`;
@@ -12,16 +17,17 @@ const BlogPage = (): ReactElement => {
   return (
     <AContainer col>
       <AHeading>Erin Tallard Fitness</AHeading>
-      {arr.length &&
-        arr.map((item, index) => (
-          <ASection key={index}>
+      {props.posts?.map((post, index) => (
+        <ALink key={index} href={`/blog/${post.slug.current}`}>
+          <ASection>
             <AHeading type={'h2'} as={'h5'}>
-              Blog Title
+              {post.title}
             </AHeading>
-            <AHeading type={'subtitle-2'}>Erin Tallard Fitness</AHeading>
-            <AText as={'overline'}>{bodySample}</AText>
+            <AHeading type={'subtitle-2'}>{post.publishedAt}</AHeading>
+            <BlockContent blocks={post.previewRaw} />
           </ASection>
-        ))}
+        </ALink>
+      ))}
     </AContainer>
   );
 };
@@ -29,9 +35,16 @@ const BlogPage = (): ReactElement => {
 export const getStaticProps = async (context): Promise<any> => {
   const client = getGraphqlClient();
 
+  const { data } = await client.query({
+    query: AllPostPreviewDocument,
+  });
+
   return {
-    props: {},
+    props: {
+      posts: data.allPost,
+      ...(await getLinks(client)),
+    },
   };
 };
 
-export default BlogPage;
+export default withLayout(BlogPage);
