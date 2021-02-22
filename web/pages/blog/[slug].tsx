33535df320e56f8b38697e35f00738fe926d6a3a
Christian Farris <christianfarriscs@gmail.com>
2021-02-21 19:57:45 -0600

    post page testing

--- web/pages/blog/[slug].tsx
+++ web/pages/blog/[slug].tsx
@@ -1,38 +1,42 @@
 import React, { ReactElement } from 'react';
 import { GetStaticProps, GetStaticPaths } from 'next';
-
+import BlockContent from '@sanity/block-content-to-react';
 import { AContainer, AHeading, ASection, AText } from 'aspire-components-react';
 
 import { getGraphqlClient } from '../../graphql/utils';
-import { AllSlugsDocument } from '../../graphql/generated';
+import { AllPostPostDocument, AllSlugsDocument } from '../../graphql/generated';
+import withLayout from '../../components/App/withLayout';
+import { getLinks } from '../../utils/menu';
 
-const PostPage = (): ReactElement => {
-  const arr = ['asdf', 'asdf', 'asdf', 'asdf', 'asdf'];
-
-  const bodySample = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel mi et eros malesuada sagittis sodales at mi. Fusce nec felis finibus, faucibus odio et, mollis ligula. Vestibulum posuere gravida ipsum nec accumsan. Donec non interdum libero, at tempor mauris. Praesent ex nunc, rutrum a orci quis, imperdiet vestibulum metus. Vestibulum non velit justo. Praesent consectetur mattis augue at rutrum. Ut vitae mattis sapien. Curabitur tristique dolor sit amet massa.`;
+const PostPage = (props): ReactElement => {
+  const post = props?.post;
 
   return (
     <AContainer col>
-      <AHeading>Erin Tallard Fitness</AHeading>
-      {arr.length &&
-        arr.map((item, index) => (
-          <ASection key={index}>
-            <AHeading type={'h2'} as={'h5'}>
-              Blog Title
-            </AHeading>
-            <AHeading type={'subtitle-2'}>Erin Tallard Fitness</AHeading>
-            <AText as={'overline'}>{bodySample}</AText>
-          </ASection>
-        ))}
+      <AHeading>{post?.title}</AHeading>
+      <BlockContent blocks={post.bodyRaw} />
     </AContainer>
   );
 };
 
 export const getStaticProps: GetStaticProps = async (context): Promise<any> => {
   const client = getGraphqlClient();
+  const { slug } = context.params;
+
+  const { data } = await client.query({
+    query: AllPostPostDocument,
+    variables: {
+      slug,
+    },
+  });
+
+  console.log('data', data);
 
   return {
-    props: {},
+    props: {
+      post: data.allPost?.[0] || null,
+      ...(await getLinks(client)),
+    },
   };
 };
 
@@ -51,4 +55,4 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export default PostPage;
+export default withLayout(PostPage);
