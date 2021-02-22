8a7301322a5e811ba3073933f88c8197f9a1e96e
Christian Farris <christianfarriscs@gmail.com>
2021-02-21 19:57:36 -0600

    state change testing

--- web/pages/contact.tsx
+++ web/pages/contact.tsx
@@ -3,20 +3,29 @@ import React, { ReactElement, useState } from 'react';
 import { AContainer, AHeading, AInput } from 'aspire-components-react';
 
 const Contact = (): ReactElement => {
-  const [input, setInput] = useState({
-    email: '',
-    message: '',
-    name: '',
-    subject: '',
-  });
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
 
   const sendEmail = async () => {
+    console.log({
+      name,
+      email,
+      subject,
+      message,
+    });
     const data = await fetch('https://www.erintallardfitness.com/.netlify/functions/email', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(input),
+      body: JSON.stringify({
+        name,
+        email,
+        subject,
+        message,
+      }),
     });
 
     console.log('data', data);
@@ -28,26 +37,26 @@ const Contact = (): ReactElement => {
       <AInput
         label={'Name'}
         placeholder={'example@example.com'}
-        value={input.name}
-        onChange={(e) => setInput({ ...input, name: e.target.value })}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
       <AInput
         label={'Email'}
         placeholder={'example@example.com'}
-        value={input.email}
-        onChange={(e) => setInput({ ...input, email: e.target.value })}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <AInput
         label={'Subject'}
         placeholder={'example@example.com'}
-        value={input.subject}
-        onChange={(e) => setInput({ ...input, subject: e.target.value })}
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
       />
       <AInput
         label={'Message'}
         placeholder={'example@example.com'}
-        value={input.message}
-        onChange={(e) => setInput({ ...input, message: e.target.value })}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
       />
       <button onClick={sendEmail}>Here</button>
     </AContainer>
