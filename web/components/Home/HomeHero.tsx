ef4397fa5fc42004009f0cdac6052a5fe98251ac
Christian Farris <christianfarriscs@gmail.com>
2021-02-21 19:57:01 -0600

    HomeHero testing

--- web/components/Home/HomeHero.tsx
+++ web/components/Home/HomeHero.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { AHero, AHeading } from 'aspire-components-react';
 import { HeroSection } from '../../types/home';
 
+import styles from './HomeHero.module.css';
+
 const HomeHero: React.FC<HeroSection> = (section: HeroSection) => (
-  <AHero key={section._key} backgroundImage={section.heroImage?.asset.url}>
+  <AHero key={section._key} backgroundImage={section.heroImage?.asset.url} className={styles.homeHero}>
     <AHeading>{section.heading}</AHeading>
   </AHero>
 );
