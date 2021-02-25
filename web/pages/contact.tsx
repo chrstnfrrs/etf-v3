b90a4d0294bbfe2c97514feafec46c6cb8a3c797
Christian Farris <christianfarriscs@gmail.com>
2021-02-24 19:51:52 -0600

    contact page things

--- web/pages/contact.tsx
+++ web/pages/contact.tsx
@@ -1,66 +1,70 @@
 import React, { ReactElement, useState } from 'react';
+import { GetStaticProps } from 'next';
+import { ABox, AContainer, AHeading, AInput } from 'aspire-components-react';
 
-import { AContainer, AHeading, AInput } from 'aspire-components-react';
+import withLayout from '../components/App/withLayout';
+import { getGraphqlClient } from '../graphql/utils';
+import { getLinks } from '../utils/menu';
 
 const Contact = (): ReactElement => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
+  const [input, setInput] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
 
-  const sendEmail = async () => {
-    console.log({
-      name,
-      email,
-      subject,
-      message,
-    });
-    const data = await fetch('https://www.erintallardfitness.com/.netlify/functions/email', {
+  const sendEmail = async () =>
+    await fetch('https://www.erintallardfitness.com/.netlify/functions/email', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        name,
-        email,
-        subject,
-        message,
-      }),
+      body: JSON.stringify(input),
     });
-
-    console.log('data', data);
-  };
 
   return (
     <AContainer>
       <AHeading>Contact Us</AHeading>
-      <AInput
-        label={'Name'}
-        placeholder={'example@example.com'}
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <AInput
-        label={'Email'}
-        placeholder={'example@example.com'}
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <AInput
-        label={'Subject'}
-        placeholder={'example@example.com'}
-        value={subject}
-        onChange={(e) => setSubject(e.target.value)}
-      />
-      <AInput
-        label={'Message'}
-        placeholder={'example@example.com'}
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={sendEmail}>Here</button>
+      <ABox width={'512'}>
+        <AInput
+          label={'Name'}
+          placeholder={'Name'}
+          value={input.name}
+          onChange={(e) => setInput({ ...input, name: e.target.value })}
+        />
+        <AInput
+          label={'Email'}
+          placeholder={'Email'}
+          value={input.email}
+          onChange={(e) => setInput({ ...input, email: e.target.value })}
+        />
+        <AInput
+          label={'Subject'}
+          placeholder={'Subject'}
+          value={input.subject}
+          onChange={(e) => setInput({ ...input, subject: e.target.value })}
+        />
+        <AInput
+          label={'Message'}
+          placeholder={'Message'}
+          value={input.message}
+          onChange={(e) => setInput({ ...input, message: e.target.value })}
+        />
+        <button onClick={sendEmail}>Here</button>
+      </ABox>
     </AContainer>
   );
 };
 
-export default Contact;
+export const getStaticProps: GetStaticProps = async () => {
+  const client = getGraphqlClient();
+
+  return {
+    props: {
+      ...(await getLinks(client)),
+    },
+  };
+};
+
+export default withLayout(Contact);
