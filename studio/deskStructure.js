a823ff0bd86ca489cbb74b77fcf4cb387d177629
Christian Farris <christianfarriscs@gmail.com>
2021-02-24 18:32:06 -0600

    Swap menu singleton to application settings

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,20 +1,22 @@
 import S from "@sanity/desk-tool/structure-builder";
- 
+import { MdSettings } from 'react-icons/md'
 export default () =>
   S.list()
     .title('Content')
     .items([
       S.listItem()
-        .title('Menu')
+        .title('Application Settings')
+        .icon(MdSettings)
         .child(
           S.document()
-            .schemaType('menu')
-            .documentId('homemenuPage')
+            .title('Application Settings')
+            .schemaType('settings')
+            .documentId('appSettings')
         ),
       
       // Add a visual divider (optional)
       S.divider(),
       // List out the rest of the document types, but filter out the config type
       ...S.documentTypeListItems()
-        .filter(listItem => !['menu', 'link', 'hero', 'button'].includes(listItem.getId()))
+        .filter(listItem => !['settings', 'links', 'link', 'hero', 'button'].includes(listItem.getId()))
     ])
