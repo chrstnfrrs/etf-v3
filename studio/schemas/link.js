bf856e58d8e9f6e9e6a5e1bf24f4ff19bad22e60
Christian Farris <christianfarriscs@gmail.com>
2021-02-24 19:09:06 -0600

    extract linkOptions to object

--- studio/schemas/link.js
+++ studio/schemas/link.js
@@ -20,49 +20,7 @@ export default {
     {
       name: 'linkOptions',
       title: 'Link Options',
-      type: 'object',
-      options: {
-        collapsible: true
-      },
-      fields: [
-        {
-          name: 'size',
-          title: 'Font Size',
-          type: 'string',
-          options: {
-            list: [
-              { title: '12px', value: '12' },
-              { title: '14px', value: '14' },
-              { title: '16px', value: '16' },
-              { title: '20px', value: '20' },
-              { title: '24px', value: '24' },
-            ]
-          },
-        },
-        {
-          name: 'weight',
-          title: 'Font Weight',
-          type: 'string',
-          options: {
-            list: [
-              { title: 'Regular', value: 'regular' },
-              { title: 'Medium', value: 'medium' },
-              { title: 'Semi-Bold', value: 'semi-bold' },
-              { title: 'Bold', value: 'bold' },
-            ]
-          },
-        },
-        {
-          name: 'decorator',
-          title: 'Decorator',
-          type: 'string',
-          options: {
-            list: [
-              { title: 'Underline', value: 'underline' },
-            ]
-          },
-        },
-      ]
+      type: 'linkOptions',
     },
   ],
   preview: {
