ef90e3bb1d329a11b723abe8bdd9df27b25239bc
Christian Farris <christianfarriscs@gmail.com>
2021-03-27 22:54:50 -0500

    eleminate unnecessary code branch

--- web/graphql/utils.ts
+++ web/graphql/utils.ts
@@ -1,15 +1,11 @@
-import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
 
-let client;
-
-export const getGraphqlClient = (): ApolloClient<NormalizedCacheObject> => {
-  if (!client) {
-    console.log('Creating a GQL Client');
-    client = new ApolloClient({
-      uri: process.env.SANITY_URL,
-      cache: new InMemoryCache(),
-    });
-  }
-
-  return client;
-};
+export const getGraphqlClient = (): ApolloClient<NormalizedCacheObject> =>
+  new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: process.env.SANITY_URL,
+  });
