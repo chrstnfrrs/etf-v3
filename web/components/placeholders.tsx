6c5b8a025b4bbb92e7edf00fb4cdb59a0ea5ed4f
Christian Farris <christianfarriscs@gmail.com>
2021-03-27 22:53:02 -0500

    create placeholder components, @TODO: replace with Aspire Components

---
+++ web/components/placeholders.tsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+
+const PlaceholderNav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  background: #e5e5e5;
+  padding: 0 16px;
+`;
+
+const PlaceholderLink = styled.a`
+  color: #101010;
+  padding: 16px 8px;
+`;
+
+const PlaceholderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 64px 128px;
+`;
+
+const PlaceholderBox = styled.div`
+  display: flex;
+`;
+
+const PlaceholderH1 = styled.h1``;
+
+const PlaceholderForm = styled.form``;
+
+export {
+  PlaceholderNav,
+  PlaceholderLink,
+  PlaceholderContainer,
+  PlaceholderBox,
+  PlaceholderH1,
+  PlaceholderForm,
+};
