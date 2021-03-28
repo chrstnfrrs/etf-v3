5a3679411c0b546c3452b70c5c88b657478d83d4
Christian Farris <christianfarriscs@gmail.com>
2021-03-27 22:58:01 -0500

    delete files

--- web/tests/utils/testing.spec.ts
+++
@@ -1,9 +0,0 @@
-import { example } from '../../utils/example';
-
-describe('testing', () => {
-  describe('example', () => {
-    test('should be true', () => {
-      expect(example()).toBe(true);
-    });
-  });
-});
