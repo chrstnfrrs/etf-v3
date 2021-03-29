5b5eda2bb4f9e6c18c5f1b560dfa1f28f0957450
Christian Farris <christianfarriscs@gmail.com>
2021-03-28 20:34:53 -0500

    start working out contact page

--- web/components/App/page.tsx
+++ web/components/App/page.tsx
@@ -4,7 +4,16 @@ import Head from 'next/head';
 import { IHomeProps } from '../../types/app';
 import { ContactFormOrHeroOrPageHeading } from '../../graphql/generated';
 import { ContactForm } from '../cookbook/contact-form';
-import { PlaceholderH1, PlaceholderContainer } from '../placeholders';
+import {
+  PlaceholderH1,
+  PlaceholderH3,
+  PlaceholderContainer,
+  PlaceholderRow,
+  PlaceholderCol,
+  PlaceholderBox,
+  PlaceholderChip,
+  PlaceholderSpacer,
+} from '../placeholders';
 
 const Page: React.FC<IHomeProps> = ({ page, sections }) => {
   return (
@@ -19,14 +28,33 @@ const Page: React.FC<IHomeProps> = ({ page, sections }) => {
           | React.ReactChild
           | undefined => {
           if (section.__typename === 'ContactForm') {
-            return <ContactForm key={section._key} />;
-          }
-
-          if (section.__typename === 'PageHeading') {
             return (
-              <PlaceholderH1 key={section._key}>{section.title}</PlaceholderH1>
+              <PlaceholderRow>
+                <PlaceholderCol justify='space-between'>
+                  <PlaceholderBox direction='column'>
+                    <PlaceholderH1>{'Get in touch!'}</PlaceholderH1>
+                    <PlaceholderH3>
+                      {"Let's work together to make a sustainable change!"}
+                    </PlaceholderH3>
+                  </PlaceholderBox>
+                  <PlaceholderSpacer />
+                  <PlaceholderRow justify='flex-start'>
+                    <PlaceholderChip>{'Facebook'}</PlaceholderChip>
+                    <PlaceholderChip>{'Instagram'}</PlaceholderChip>
+                  </PlaceholderRow>
+                </PlaceholderCol>
+                <PlaceholderCol>
+                  <ContactForm key={section._key} submit={section.submit} />
+                </PlaceholderCol>
+              </PlaceholderRow>
             );
           }
+
+          // if (section.__typename === 'PageHeading') {
+          //   return (
+          // <PlaceholderH1 key={section._key}>{section.title}</PlaceholderH1>
+          //   );
+          // }
 
           return undefined;
         })}
