848f2e4d0d5192d1c41970d3e78e5985598be003
Christian Farris <christianfarriscs@gmail.com>
2021-05-31 20:20:00 -0500

    create types

---
+++ web/types/index.d.ts
@@ -0,0 +1,10 @@
+import * as Apollo from './apollo.d';
+import * as App from './app.d';
+import * as Sanity from './sanity.d';
+
+/* eslint-disable */
+// @ts-ignore
+type AllowAny = any;
+/* eslint-enable */
+
+export { AllowAny, Apollo, App, Sanity };
