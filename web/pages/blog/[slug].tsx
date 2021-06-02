d23767d9fb243f920ece2570d9d6dfc438672a46
Christian Farris <christianfarriscs@gmail.com>
2021-06-01 22:20:12 -0500

    reset Blog slug page

--- web/pages/blog/[slug].tsx
+++ web/pages/blog/[slug].tsx
@@ -1,82 +1,95 @@
 import React from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import { format } from 'date-fns';
 
-import withLayout from '../../components/app/with-layout';
-import { getGraphqlClient } from '../../graphql/utils';
-import { getLinks } from '../../repositories/menu';
-import { getPost, getAllPostSlugs } from '../../repositories/posts';
-import { Post } from '../../graphql/generated';
-import { IPostPageProps } from '../../types/app';
-import {
-  PlaceholderContainer,
-  PlaceholderH1,
-  PlaceholderH3,
-} from '../../components/placeholders';
-import { PlaceholderBlock } from '../../components/cookbook/block';
+import * as GraphqlClient from '../../graphql/graphql-client';
+import * as Codegen from '../../graphql/generated';
+import * as MenuRepository from '../../repositories/menu';
 
-const PostPage: React.FC<IPostPageProps> = ({ page, ...props }) => {
-  return (
-    <>
-      <Head>
-        <title>{page.title}</title>
-        <meta content={page.title as string} key='title' property='og:title' />
-        <meta content={page.description as string} name='description' />
-      </Head>
-      <PlaceholderContainer>
-        <Image
-          alt={page.mainImage?.alt as string}
-          height='100vw'
-          layout='intrinsic'
-          src={page.mainImage?.asset?.url as string}
-          width='100vw'
-        />
-        <PlaceholderH1>{page.title}</PlaceholderH1>
-        <PlaceholderH3>
-          {format(new Date(page.publishedAt), 'MMM do, yyyy')}
-        </PlaceholderH3>
-        <PlaceholderBlock content={page.bodyRaw} />
-      </PlaceholderContainer>
-    </>
-  );
+type Page = {
+  title: string;
 };
 
+type Props = {
+  page: Page;
+};
+
+type StaticProps = {
+  props: Props;
+};
+
+const PostPage: React.FC<Props> = () => null;
+//   return (
+//     <>
+//       <Head>
+//         <title>{page.title}</title>
+//         <meta content={page.title as string} key='title' property='og:title' />
+//         <meta content={page.description as string} name='description' />
+//       </Head>
+//       <PlaceholderContainer>
+//         <Image
+//           alt={page.mainImage?.alt as string}
+//           height='100vw'
+//           layout='intrinsic'
+//           src={page.mainImage?.asset?.url as string}
+//           width='100vw'
+//         />
+//         <PlaceholderH1>{page.title}</PlaceholderH1>
+//         <PlaceholderH3>
+//           {format(new Date(page.publishedAt), 'MMM do, yyyy')}
+//         </PlaceholderH3>
+//         <PlaceholderBlock content={page.bodyRaw} />
+//       </PlaceholderContainer>
+//     </>
+//   );
+// };
+
 const getStaticPaths: GetStaticPaths = async () => {
-  const client = getGraphqlClient();
+  const client = GraphqlClient.get();
 
-  const allSlugs = await getAllPostSlugs(client);
+  const {
+    data: { allPost },
+  } = await client.query({
+    query: Codegen.AllSlugsDocument,
+  });
 
-  const paths = allSlugs
-    .filter((post: Post) => post.slug?.current?.length)
-    .map((post: Post) => ({
+  const paths = allPost
+    .filter((post: Codegen.Post) => post.slug?.current?.length)
+    .map((post: Codegen.Post) => ({
       params: {
         slug: post.slug?.current as string,
       },
     }));
 
   return {
-    fallback: false, // See the "fallback" section below
+    fallback: false,
     paths,
   };
 };
 
 const getStaticProps: GetStaticProps = async ({ params }) => {
-  const client = getGraphqlClient();
+  const client = GraphqlClient.get();
 
   const slug = `${(params?.slug as string) || ''}`;
 
-  const post = await getPost(client, slug);
+  const {
+    data: {
+      allPost: [page],
+    },
+  } = await client.query({
+    query: Codegen.AllPostPostDocument,
+    variables: {
+      slug,
+    },
+  });
 
   return {
     props: {
-      page: post,
-      ...(await getLinks(client)),
+      page,
+      ...(await MenuRepository.getLinks(client)),
     },
   };
 };
 
 export { getStaticPaths, getStaticProps };
-
-export default withLayout(PostPage);
+export type { StaticProps, Props, Page };
+export default PostPage;
