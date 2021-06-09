50189d2f6820021118ce6106ad3cfb3eb99cfd93
Christian Farris <christianfarriscs@gmail.com>
2021-06-08 23:08:03 -0500

    make blog/[slug] render

--- web/pages/blog/[slug].tsx
+++ web/pages/blog/[slug].tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import styled from '@emotion/styled';
+import * as DateFns from 'date-fns';
 
+import * as Types from '../../types/index.d';
 import * as GraphqlClient from '../../graphql/graphql-client';
 import * as Codegen from '../../graphql/generated';
 import * as MenuRepository from '../../repositories/menu';
+import { PlaceholderBlock } from '../../components/cookbook/block';
+import { ETFLayout } from '../../components/etf/etf-layout';
 
 type Page = {
   title: string;
+  publishedAt: Date;
+  mainImage: {
+    alt: string;
+    asset: {
+      url: string;
+    };
+  };
+  bodyRaw: Types.Sanity.Block[];
 };
 
 type Props = {
@@ -17,31 +30,29 @@ type StaticProps = {
   props: Props;
 };
 
-const PostPage: React.FC<Props> = () => null;
-//   return (
-//     <>
-//       <Head>
-//         <title>{page.title}</title>
-//         <meta content={page.title as string} key='title' property='og:title' />
-//         <meta content={page.description as string} name='description' />
-//       </Head>
-//       <PlaceholderContainer>
-//         <Image
-//           alt={page.mainImage?.alt as string}
-//           height='100vw'
-//           layout='intrinsic'
-//           src={page.mainImage?.asset?.url as string}
-//           width='100vw'
-//         />
-//         <PlaceholderH1>{page.title}</PlaceholderH1>
-//         <PlaceholderH3>
-//           {format(new Date(page.publishedAt), 'MMM do, yyyy')}
-//         </PlaceholderH3>
-//         <PlaceholderBlock content={page.bodyRaw} />
-//       </PlaceholderContainer>
-//     </>
-//   );
-// };
+const AH1 = styled.h1``;
+const AH3 = styled.h3``;
+const AImage = styled.img`
+  border-radius: 4px 4px 0 0;
+  height: 35vh !important;
+`;
+
+const PostPage: React.FC<Props> = (props) => {
+  return (
+    <ETFLayout {...props}>
+      <AImage
+        alt={props.page.mainImage.alt}
+        className='a-image'
+        src={props.page.mainImage.asset?.url}
+      />
+      <AH1>{props.page.title}</AH1>
+      <AH3>
+        {DateFns.format(new Date(props.page.publishedAt), 'MMM do, yyyy')}
+      </AH3>
+      <PlaceholderBlock content={props.page.bodyRaw} />
+    </ETFLayout>
+  );
+};
 
 const getStaticPaths: GetStaticPaths = async () => {
   const client = GraphqlClient.get();
@@ -73,7 +84,7 @@ const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const {
     data: {
-      allPost: [page],
+      allPost: [pageReal],
     },
   } = await client.query({
     query: Codegen.AllPostPostDocument,
@@ -82,10 +93,16 @@ const getStaticProps: GetStaticProps = async ({ params }) => {
     },
   });
 
+  const page = {
+    description: 'asdf',
+    title: 'asdf',
+    ...pageReal,
+  };
+
   return {
     props: {
+      menu: await MenuRepository.getLinks(client),
       page,
-      ...(await MenuRepository.getLinks(client)),
     },
   };
 };
