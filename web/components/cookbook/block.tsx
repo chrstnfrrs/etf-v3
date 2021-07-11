c2e30a3fbc912a472dd00d0a7980edfa714a6ea0
Christian Farris <christianfarriscs@gmail.com>
2021-07-11 01:39:08 -0500

    add aspire-component-react headings to block renderer

--- web/components/cookbook/block.tsx
+++ web/components/cookbook/block.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import BlockContent from '@sanity/block-content-to-react';
-import styled from '@emotion/styled';
+import {
+  AH1,
+  AH2,
+  AH3,
+  AH4,
+  AH5,
+  AH6,
+  ASubtitle,
+} from 'aspire-components-react';
 
 import { AllowAny } from '../../types/index.d';
-
-const PlaceholderH1 = styled.h1`
-  line-height: 2;
-`;
-const PlaceholderH3 = styled.h3`
-  line-height: 1.5;
-`;
 
 const BlockRenderer = (props: AllowAny) => {
   const { style = 'normal' } = props.node;
 
   if (style === 'h1') {
-    return <PlaceholderH1>{props.children}</PlaceholderH1>;
+    return <AH1 type='strong'>{props.children}</AH1>;
+  }
+
+  if (style === 'h2') {
+    return <AH2 type='strong'>{props.children}</AH2>;
   }
 
   if (style === 'h3') {
-    return <PlaceholderH3>{props.children}</PlaceholderH3>;
+    return <AH3 type='strong'>{props.children}</AH3>;
+  }
+
+  if (style === 'h4') {
+    return <AH4 type='strong'>{props.children}</AH4>;
+  }
+
+  if (style === 'h5') {
+    return <AH5 type='strong'>{props.children}</AH5>;
+  }
+
+  if (style === 'h6') {
+    return <AH6 type='strong'>{props.children}</AH6>;
+  }
+
+  if (style === 'subtitle-1') {
+    return <ASubtitle>{props.children}</ASubtitle>;
+  }
+
+  if (style === 'subtitle-2') {
+    return <ASubtitle as='subtitle-2'>{props.children}</ASubtitle>;
   }
 
   return BlockContent.defaultSerializers.types.block(props);
