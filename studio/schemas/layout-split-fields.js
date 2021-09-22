ddcf85ef65c8835ee762033c18ff90fc8df3a45f
Christian Farris <christianfarriscs@gmail.com>
2021-09-21 22:27:34 -0500

    feat: layoutSplitFields can display mainImage or contentMain

--- studio/schemas/layout-split-fields.js
+++ studio/schemas/layout-split-fields.js
@@ -1,29 +1,30 @@
 export default {
   fields: [
     {
-      name: 'type',
-      of: [{ type: 'link' }],
-      title: 'Display Type',
-      validation: (Rule) => Rule.required(),
+      name: 'display',
+      of: [{ type: 'mainImage' }, { type: 'contentMain' }],
+      title: 'Display',
+      type: 'array',
+      validation: (Rule) => Rule.min(1).max(1).error('Must have one item'),
     },
-    {
-      name: 'mobile',
-      options: {
-        list: [
-          {
-            title: 'Top',
-            value: 'top',
-          },
-          {
-            title: 'Bottom',
-            value: 'bottom',
-          },
-        ],
-      },
-      title: 'Mobile Display Order',
-      type: 'string',
-      validation: (Rule) => Rule.required(),
-    },
+    // {
+    //   name: 'mobile',
+    //   options: {
+    //     list: [
+    //       {
+    //         title: 'Top',
+    //         value: 'top',
+    //       },
+    //       {
+    //         title: 'Bottom',
+    //         value: 'bottom',
+    //       },
+    //     ],
+    //   },
+    //   title: 'Mobile Display Order',
+    //   type: 'string',
+    //   validation: (Rule) => Rule.required(),
+    // },
   ],
   name: 'layoutSplitFields',
   title: 'Split Layout Fields',
