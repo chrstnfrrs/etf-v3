52b20999dc0ff02f3538686ff700cfb50690fcf4
Christian Farris <christianfarriscs@gmail.com>
2021-10-17 21:14:12 -0500

    feat: hide new types

--- studio/desk-structure.js
+++ studio/desk-structure.js
@@ -66,6 +66,7 @@ export default () =>
             'sectionTestimonial',
             'testimonial',
             'pages',
+            'layoutSplitContent',
           ].includes(listItem.getId()),
       ),
     ]);
