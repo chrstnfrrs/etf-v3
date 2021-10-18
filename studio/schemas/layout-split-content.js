e77d8d0ae8f0f04281c0bce7d2962520c493f79d
Christian Farris <christianfarriscs@gmail.com>
2021-10-17 21:13:24 -0500

    feat: create layout split content schema type

---
+++ studio/schemas/layout-split-content.js
@@ -0,0 +1,22 @@
+export default {
+  fields: [
+    {
+      name: 'heading',
+      title: 'Heading',
+      type: 'string',
+    },
+    {
+      name: 'subheading',
+      title: 'Sub Heading',
+      type: 'string',
+    },
+    {
+      name: 'link',
+      title: 'Link',
+      type: 'link',
+    },
+  ],
+  name: 'layoutSplitContent',
+  title: 'Split Layout Content',
+  type: 'document',
+};
