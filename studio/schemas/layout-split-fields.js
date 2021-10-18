187b13bcf8ec94f0812c848b98884434e23fa136
Christian Farris <christianfarriscs@gmail.com>
2021-10-17 21:13:41 -0500

    feat: lift content to new type and allow for mobile positioning

--- studio/schemas/layout-split-fields.js
+++ studio/schemas/layout-split-fields.js
@@ -3,41 +3,35 @@ export default {
     {
       name: 'display',
       of: [
-        { type: 'mainImage' },
         {
-          fields: [
-            {
-              name: 'content',
-              title: 'Content',
-              type: 'contentMain',
-            },
-          ],
-          name: 'content',
-          type: 'object',
+          type: 'mainImage',
+        },
+        {
+          type: 'layoutSplitContent',
         },
       ],
       title: 'Display',
       type: 'array',
       validation: (Rule) => Rule.min(1).max(1).error('Must have one item'),
     },
-    // {
-    //   name: 'mobile',
-    //   options: {
-    //     list: [
-    //       {
-    //         title: 'Top',
-    //         value: 'top',
-    //       },
-    //       {
-    //         title: 'Bottom',
-    //         value: 'bottom',
-    //       },
-    //     ],
-    //   },
-    //   title: 'Mobile Display Order',
-    //   type: 'string',
-    //   validation: (Rule) => Rule.required(),
-    // },
+    {
+      name: 'mobile',
+      options: {
+        list: [
+          {
+            title: 'Top',
+            value: 'top',
+          },
+          {
+            title: 'Bottom',
+            value: 'bottom',
+          },
+        ],
+      },
+      title: 'Mobile Display Order',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
   ],
   name: 'layoutSplitFields',
   title: 'Split Layout Fields',
