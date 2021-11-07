2d41e198a2f379e169ca993427bc2c77a8fad359
Christian Farris <christianfarriscs@gmail.com>
2021-11-07 00:45:17 -0500

    refactor: move ssr code to repo layer

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,38 +6,26 @@ import { ETFLayout } from '../components/etf/etf-layout';
 import { PageSerializer } from '../components/sanity/component-serializer';
 // Static Prop Imports
 import * as GraphqlClient from '../graphql/graphql-client';
+import * as HomePageRepository from '../repositories/home-repository';
 import * as MenuRepository from '../repositories/menu';
-import { HomePageDocument } from '../graphql/generated';
 
-const BlogPage = ({ page, data, ...props }) => {
+const BlogPage = ({ menu, page }) => {
   return (
-    <ETFLayout page={page} {...props}>
-      <PageSerializer data={data} sections={page.sections} />
+    <ETFLayout menu={menu} page={page}>
+      <PageSerializer sections={page.sections} />
     </ETFLayout>
   );
 };
 
 const getStaticProps = async () => {
   const client = GraphqlClient.get();
-
-  // Fetch page info
-  const {
-    data: { allHomePage },
-  } = await client.query({
-    query: HomePageDocument,
-  });
-
-  const [page] = allHomePage;
-
-  console.log('page', page);
-  console.log('page sections', page.sections);
+  const page = await HomePageRepository.getHomePage(client);
+  const menu = await MenuRepository.getLinks(client);
 
   return {
     props: {
-      data: null,
-      menu: await MenuRepository.getLinks(client),
+      menu,
       page,
-      // posts: allPost,
     },
   };
 };
